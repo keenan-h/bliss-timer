5fb097442055200dd37347f2efabc97c0001ffc7
keenan-h <keenanhurley@gmail.com>
2020-12-30 21:22:29 -0500

    first commit

---
+++ bliss_timer.py
@@ -0,0 +1,25 @@
+# need to have ffmpeg installed
+# only works with files that are exported at a 44.1k sample rate & a bit depth of 16
+
+import time
+from pydub import AudioSegment
+from pydub.playback import play
+
+minutes = int(input("\nHow many minutes?\n"))
+print("\n")
+while minutes == 0:
+    sound = AudioSegment.from_file("./sounds/alarmsound1_44_16_mono.wav", format="wav")
+    quieter_sound = sound - 5
+    play(quieter_sound)
+
+while minutes > 1:
+    print(str(minutes) + " minutes left.\n")
+    time.sleep(60)
+    minutes -= 1
+
+print(str(minutes) + " minute left.\n")
+time.sleep(60)
+
+sound = AudioSegment.from_file("./sounds/alarmsound1_44_16_mono.wav", format="wav")
+quieter_sound = sound - 5
+play(quieter_sound)
---
+++ findfile1.py
@@ -0,0 +1,4 @@
+import glob
+sound1lst = glob.glob("./alarmsound1_44_16_mono.wav")
+sound1 = sound1lst[0]
+print(f"{sound1!r}")
